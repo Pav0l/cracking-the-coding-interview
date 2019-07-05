b49dcfceab87f3d442169de5be0b50ef03d6ec17
Pav0l <pavol.karas@gmail.com>
2019-07-05 11:31:54 +0200

    Move nodes into node.py

--- Linked_lists/node.py
+++ Linked_lists/node.py
@@ -8,3 +8,21 @@ class Node:
         self.value = value
         # set ref to the next node in the linked list
         self.next_node = next_node
+
+
+n0 = Node(10)
+n1 = Node(6)
+n2 = Node(5)
+n3 = Node(6)
+n4 = Node(7)
+n5 = Node(9)
+n6 = Node(14)
+n7 = Node(21)
+
+n0.next_node = n1
+n1.next_node = n2
+n2.next_node = n3
+n3.next_node = n4
+n4.next_node = n5
+n5.next_node = n6
+n6.next_node = n7
--- Linked_lists/removeDuplicates.py
+++ Linked_lists/removeDuplicates.py
@@ -1,18 +1,18 @@
 """
 Remove Dups: Write code to remove duplicates from an unsorted linked list.
 """
-from node import Node
+from node import n0
 
-n0 = Node(10)
-n1 = Node(6)
-n2 = Node(5)
-n3 = Node(6)
-n4 = Node(7)
+# n0 = Node(10)
+# n1 = Node(6)
+# n2 = Node(5)
+# n3 = Node(6)
+# n4 = Node(7)
 
-n0.next_node = n1
-n1.next_node = n2
-n2.next_node = n3
-n3.next_node = n4
+# n0.next_node = n1
+# n1.next_node = n2
+# n2.next_node = n3
+# n3.next_node = n4
 
 
 def print_nodes(node):
