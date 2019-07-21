e2b7ec5e5e52d9c17c44ba57ad0369a64bf3831f
Pav0l <pavol.karas@gmail.com>
2019-07-21 09:35:57 +0200

    Genomic range query optimized

--- Codility/genomic_range_query.py
+++ Codility/genomic_range_query.py
@@ -50,8 +50,70 @@ def genomic_query(S, P, Q):
     return result
 
 
+# O(n + m)
+def genomic_query_two(S, P, Q):
+    N = len(S)
+    M = len(P)
+
+    # create arrays for every nucleotide
+    # that will keep track of prefix sums of nucleotides in DNA sequence
+    # N + 1, to start with initial value of 0
+    a_prefix_sum = [0] * (N + 1)
+    c_prefix_sum = [0] * (N + 1)
+    g_prefix_sum = [0] * (N + 1)
+    t_prefix_sum = [0] * (N + 1)
+
+    # get prefix sums for each nucleotide
+    for i in range(N):
+        # update next value with previous value
+        a_prefix_sum[i + 1] = a_prefix_sum[i]
+        c_prefix_sum[i + 1] = c_prefix_sum[i]
+        g_prefix_sum[i + 1] = g_prefix_sum[i]
+        t_prefix_sum[i + 1] = t_prefix_sum[i]
+
+        # check which nucleotide is in sequence next and increase its count
+        if S[i] == 'A':
+            a_prefix_sum[i+1] = a_prefix_sum[i] + 1
+        elif S[i] == 'C':
+            c_prefix_sum[i+1] = c_prefix_sum[i] + 1
+        elif S[i] == 'G':
+            g_prefix_sum[i+1] = g_prefix_sum[i] + 1
+        else:
+            t_prefix_sum[i+1] = t_prefix_sum[i] + 1
+
+    result = []
+
+    for i in range(M):
+        # calculate the difference of nucleotide sums for given sequence P[K] : Q[K]
+        res_A = a_prefix_sum[Q[i]+1] - a_prefix_sum[P[i]]
+        res_C = c_prefix_sum[Q[i]+1] - c_prefix_sum[P[i]]
+        res_G = g_prefix_sum[Q[i]+1] - g_prefix_sum[P[i]]
+
+        # if the sum is bigger than zero => the nucleotide occured in the sequence
+        if res_A > 0:
+            result.append(1)
+        elif res_C > 0:
+            result.append(2)
+        elif res_G > 0:
+            result.append(3)
+        else:
+            result.append(4)
+    return result
+
+
 s = 'CAGCCTA'
 p = [2, 5, 0]
 q = [4, 5, 6]
 
-print(genomic_query(s, p, q))
+print(genomic_query_two(s, p, q))
+
+
+s = 'C'
+p = [0]
+q = [0]
+print(genomic_query_two(s, p, q))
+
+s = 'AC'
+p = [0, 0, 1]
+q = [0, 1, 1]
+print(genomic_query_two(s, p, q))
